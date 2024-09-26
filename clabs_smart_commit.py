6294be650f2d5ff26c43199cc74ae03d78ec57bd
hari77725 <harihara.sudhan@customerlabs.co>
2024-09-26 23:59:38 +0530

    ccdp1-2324 #time 2d #comment fixed css

--- clabs_smart_commit.py
+++ clabs_smart_commit.py
@@ -97,6 +97,8 @@ def extract_transition(commit_msg: str) -> Optional[str]:
         "in_progress",
     ]
 
+    print(commit_msg)
+
     pattern = r"#(\w+)"  # Matches '# followed by one or more word characters'
     states = re.findall(pattern, commit_msg, re.IGNORECASE)
     states = list(map(str.lower, states))
